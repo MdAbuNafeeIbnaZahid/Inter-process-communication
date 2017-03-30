9b058ec7c5d3fbab7ff738d40f8bbe24d17ea822
MdAbuNafeeIbnaZahid <1305023.maniz@ugrad.cse.buet.ac.bd>
2017-03-30 17:35:24 +0600

    Created a general structure with lock and semaphore which can be used at all buffers. created a function to initialize outstanding pool

--- offline_IPC.c
+++ offline_IPC.c
@@ -20,13 +20,43 @@ struct myPool
     long long stdId[SIZE], password[SIZE];
     sem_t howMuchEmpty;
     sem_t howMuchFull;
+
 };
+struct myPool outstandingPool;
+
+void initializeOustandingPool()
+{
+    int res;
+    res = pthread_mutex_init( &(outstandingPool.myMutex), NULL );
+    if (res != 0)
+    {
+        printf("Failed to initialize mutex");
+        exit(0);
+    }
+    outstandingPool.startIdx = 1;
+    outstandingPool.endIdx = 0;
+    res = sem_init( &(outstandingPool.howMuchEmpty), 0, 10  );
+    if (res != 0)
+    {
+        printf("Failed to initialize semaphore");
+        exit(0);
+    }
+    res = sem_init( &(outstandingPool.howMuchFull), 0, 0  );
+    if (res != 0)
+    {
+        printf("Failed to initialize semaphore");
+        exit(0);
+    }
+
+}
 
 void *studentThreadFunction(void *arg); // stdId will be passed
 
 
 int main()
 {
+    initializeOustandingPool();
+
     return 0;
 }
 
